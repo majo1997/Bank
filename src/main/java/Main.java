06867685208400306fd13a6ea6aa64f134caed78
majo1997 <majo1997@github.com>
2021-03-10 07:21:00 +0100

    added er model pdf, maven dependencies, basic database connection code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,41 @@
+//todo to package
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Dotenv dotenv = Dotenv.load();
+        String x = dotenv.get("DATABASE_URL");
+        System.out.println(x);
+
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bank", "postgres", "root")) {
+
+            System.out.println("Java JDBC PostgreSQL Example");
+            // When this class first attempts to establish a connection, it automatically loads any JDBC 4.0 drivers found within
+            // the class path. Note that your application must manually load any JDBC drivers prior to version 4.0.
+//          Class.forName("org.postgresql.Driver");
+
+            System.out.println("Connected to PostgreSQL database!");
+            Statement statement = connection.createStatement();
+            System.out.println("Reading test records...");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM test");
+            while (resultSet.next()) {
+                System.out.printf("%s", resultSet.getString("name"));
+            }
+
+        } /*catch (ClassNotFoundException e) {
+            System.out.println("PostgreSQL JDBC driver not found.");
+            e.printStackTrace();
+        }*/ catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
     }
 }
